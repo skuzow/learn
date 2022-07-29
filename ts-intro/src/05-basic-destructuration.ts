d74998d5f480d3d6c19936c7f716fc45f00c812a
skuzow <alex@skuzow.net>
2022-07-29 18:11:20 +0200

    refactor: ts-intro: basic destructuration

--- ts-intro/src/05-basic-destructuration.ts
+++ ts-intro/src/05-basic-destructuration.ts
@@ -35,8 +35,6 @@ const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
 const [ p1, p2 ] = dbz;
 const [ , , p3 ] = dbz;
 
-console.log('Character 1:', p1)
-console.log('Character 2:', p2)
-console.log('Character 3:', p3)
-
-
+console.log('Character 1:', p1);
+console.log('Character 2:', p2);
+console.log('Character 3:', p3);
