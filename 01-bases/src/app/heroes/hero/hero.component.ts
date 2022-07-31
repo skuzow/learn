e9701a42417546abb9792bc619d3c5dad8f27728
skuzow <alex@skuzow.net>
2022-07-31 15:20:25 +0200

    feat: bases: heroes hero component

---
+++ 01-bases/src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-hero',
+    templateUrl: 'hero.component.html'
+})
+export class HeroComponent {
+
+    name: string = 'Ironman';
+    age: number = 45;
+
+    get capitalizedName(): string {
+        return this.name.toUpperCase();
+    }
+
+    obtainName(): string {
+        return `${ this.name } - ${ this.age }`
+    }
+
+    changeName(): void {
+        this.name = 'Spiderman';
+    }
+
+    changeAge(): void {
+        this.age = 30;
+    }
+
+}
