a17ecb71da029c1d3b4871eb9cf5d190ffb692b1
skuzow <alex@skuzow.net>
2022-08-04 11:08:04 +0200

    feat: pipes-app: menu init

--- 04-pipes-app/src/app/app.module.ts
+++ 04-pipes-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    PrimeNgModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
