498535d3a2bbf1611adade218ee0038063570db6
skuzow <alex@skuzow.net>
2022-08-08 20:28:28 +0200

    feat: heroes-app: api url environment

--- 05-heroes-app/src/app/heroes/services/heroes.service.ts
+++ 05-heroes-app/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../../../environments/environment';
 
 import { Hero } from '../interfaces/heroes.interface';
 
@@ -10,14 +11,16 @@ import { Hero } from '../interfaces/heroes.interface';
 })
 export class HeroesService {
 
+  private apiUrl: string = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this.apiUrl}/heroes`);
   }
 
   getHeroeById(id: string): Observable<Hero> {
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Hero>(`${this.apiUrl}/heroes/${id}`);
   }
 
 }
--- 05-heroes-app/src/environments/environment.prod.ts
+++ 05-heroes-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://skuzow.net/heroes/api'
 };
