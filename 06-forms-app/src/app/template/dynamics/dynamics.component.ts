d6aaf8b473a0292a1f53406e9d5ac6ae6b3bfa97
skuzow <alex@skuzow.net>
2022-08-10 01:23:34 +0200

    feat: forms-app: template dynamics form

--- 06-forms-app/src/app/template/dynamics/dynamics.component.ts
+++ 06-forms-app/src/app/template/dynamics/dynamics.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+
+interface Person {
+  name: string,
+  favorites: Favorite[]
+}
+
+interface Favorite {
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html'
 })
-export class DynamicsComponent implements OnInit {
+export class DynamicsComponent {
 
-  constructor() { }
+  newFavoriteName: string = '';
 
-  ngOnInit(): void {
+  person: Person = {
+    name: 'Alex',
+    favorites: [
+      {
+        id: 1,
+        name: 'Zelda BOTW'
+      },
+      {
+        id: 2,
+        name: 'Stardew Valley'
+      }
+    ]
+  }
+
+  @ViewChild('myForm') myForm!: NgForm;
+
+  isInvalidName() {
+    return this.myForm?.controls['name']?.touched
+        && this.myForm?.controls['name']?.errors;
+  }
+
+  addFavorite() {
+    if (this.newFavoriteName === '') return;
+    const newFavorite: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newFavoriteName
+    }
+    this.person.favorites.push({ ...newFavorite });
+    this.newFavoriteName = '';
+  }
+
+  removeFavorite(index: number) {
+    this.person.favorites.splice(index, 1);
+  }
+
+  save() {
+    console.log('Correct post');
+    this.myForm.resetForm();
   }
 
 }
