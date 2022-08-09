4e38c63ef8ecfe3ebb90621416ced60355951c06
skuzow <alex@skuzow.net>
2022-08-09 19:01:31 +0200

    feat: forms-app: components & lazyload

---
+++ 06-forms-app/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+
+@NgModule({
+  declarations: [
+    SidemenuComponent
+  ],
+  exports: [
+    SidemenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
---
+++ 06-forms-app/src/app/template/basics/basics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-basics',
+  templateUrl: './basics.component.html'
+})
+export class BasicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
