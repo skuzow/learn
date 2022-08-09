a428bfa3c71145a421f9ea99441fb113e0bc3df8
skuzow <alex@skuzow.net>
2022-08-09 20:38:44 +0200

    feat: forms-app: custom min directive

--- 06-forms-app/src/app/template/template.module.ts
+++ 06-forms-app/src/app/template/template.module.ts
@@ -7,12 +7,15 @@ import { BasicsComponent } from './basics/basics.component';
 import { DynamicsComponent } from './dynamics/dynamics.component';
 import { SwitchesComponent } from './switches/switches.component';
 
+import { CustomMinDirective } from './directives/custom-min.directive';
+
 
 @NgModule({
   declarations: [
     BasicsComponent,
     DynamicsComponent,
-    SwitchesComponent
+    SwitchesComponent,
+    CustomMinDirective
   ],
   imports: [
     CommonModule,
