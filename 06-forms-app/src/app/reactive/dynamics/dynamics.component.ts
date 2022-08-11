b82e1301ad484506a042724c70bde9c2b14cc714
skuzow <alex@skuzow.net>
2022-08-11 20:57:35 +0200

    feat: forms-app: reactive init

--- 06-forms-app/src/app/reactive/dynamics/dynamics.component.ts
+++ 06-forms-app/src/app/reactive/dynamics/dynamics.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html'
 })
-export class DynamicsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class DynamicsComponent { }
--- 06-forms-app/src/app/reactive/switches/switches.component.ts
+++ 06-forms-app/src/app/reactive/switches/switches.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SwitchesComponent { }
