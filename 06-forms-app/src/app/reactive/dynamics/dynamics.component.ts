42f0acbc557f0cb66cacdf137024616e11c8c57e
skuzow <alex@skuzow.net>
2022-08-31 13:57:23 +0200

    feat: forms-app: reactive dynamics form

--- 06-forms-app/src/app/reactive/dynamics/dynamics.component.ts
+++ 06-forms-app/src/app/reactive/dynamics/dynamics.component.ts
@@ -1,8 +1,54 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html'
 })
-export class DynamicsComponent { }
+export class DynamicsComponent {
+
+  myForm: FormGroup = this.fb.group({
+    name: [ , [ Validators.required, Validators.minLength(3) ] ],
+    favorites: this.fb.array(
+      [
+        [ 'Metal Gear', Validators.required ],
+        [ 'Death Stranding', Validators.required ]
+      ],
+      Validators.required
+    )
+  });
+
+  newFavorite: FormControl = this.fb.control('', Validators.required);
+
+  get favorites() {
+    return this.myForm.get('favorites') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  isInvalidField(field: string) {
+    return this.myForm.controls[field].touched
+        && this.myForm.controls[field].errors;
+  }
+
+  addFavorite() {
+    if (this.newFavorite.invalid) return;
+    this.favorites.push(this.fb.control(this.newFavorite.value, Validators.required));
+    this.newFavorite.reset();
+  }
+
+  deleteFavorite(index: number) {
+    this.favorites.removeAt(index);
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
+
+}
