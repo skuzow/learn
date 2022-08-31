b54a4c98b083fa5152d87722df5ee48960d069ab
skuzow <alex@skuzow.net>
2022-08-31 14:42:58 +0200

    feat: forms-app: reactive switches form

--- 06-forms-app/src/app/reactive/switches/switches.component.ts
+++ 06-forms-app/src/app/reactive/switches/switches.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent { }
+export class SwitchesComponent implements OnInit {
+
+  myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    notifications: [ true, Validators.required ],
+    termsConditions: [ false, Validators.requiredTrue ]
+  });
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset({
+      ...this.person,
+      termsConditions: false
+    });
+    this.myForm.valueChanges.subscribe(({ termsConditions, ...rest }) => {
+      this.person = rest;
+    });
+  }
+
+  save() {
+    const formValue = { ...this.myForm.value };
+    delete formValue.termsConditions;
+    this.person = formValue;
+  }
+
+}
