24dcecdbbcf6f5f9ca2c5ef52c7dd4f2b0ce44af
skuzow <alex@skuzow.net>
2022-09-20 23:27:03 +0200

    feat: forms-app: auth register name & email

--- 06-forms-app/src/app/auth/auth.module.ts
+++ 06-forms-app/src/app/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-
 
 @NgModule({
   declarations: [
@@ -14,7 +13,8 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- 06-forms-app/src/app/auth/pages/register/register.component.ts
+++ 06-forms-app/src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // temp
+  namePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"; // Validators.email not rly accurate
+
+  myForm: FormGroup = this.fb.group({
+    name: [ '', [ Validators.required, Validators.pattern(this.namePattern) ] ],
+    email: [ '', [ Validators.required, Validators.pattern(this.emailPattern) ] ]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm.reset({
+      name: 'Alejandro Porras',
+      email: 'alex@skuzow.net'
+    })
+  }
+
+  isInvalidField(field: string) {
+    return this.myForm.get(field)?.touched
+        && this.myForm.get(field)?.invalid;
+  }
+
+  save() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
   }
 
 }
