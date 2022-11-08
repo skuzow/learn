db2c7600138303de112e1863eea38dfaf1fa2c49
skuzow <alex@skuzow.net>
2022-11-08 23:25:51 +0100

    feat: selectors-app: countries selector basis

--- 07-selectors-app/src/app/countries/countries.module.ts
+++ 07-selectors-app/src/app/countries/countries.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
@@ -11,6 +12,7 @@ import { SelectorPageComponent } from './pages/selector-page/selector-page.compo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     CountriesRoutingModule
   ]
 })
--- 07-selectors-app/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 07-selectors-app/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    region: [ '', Validators.required ]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  save() {
+    console.log(this.myForm.value)
   }
 
 }
