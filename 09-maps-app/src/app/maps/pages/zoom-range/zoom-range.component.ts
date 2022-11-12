5f4b4962249270706132847e58b47de7136285fd
skuzow <alex@skuzow.net>
2022-11-12 03:01:32 +0100

    feat: maps-app: components & routes

---
+++ 09-maps-app/src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-zoom-range',
+  templateUrl: './zoom-range.component.html'
+})
+export class ZoomRangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
