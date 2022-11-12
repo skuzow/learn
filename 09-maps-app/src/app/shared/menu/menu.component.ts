cd1a9513444018cdda2b78341ba0f8022ac8b816
skuzow <alex@skuzow.net>
2022-11-12 03:22:05 +0100

    feat: maps-app: menu

--- 09-maps-app/src/app/shared/menu/menu.component.ts
+++ 09-maps-app/src/app/shared/menu/menu.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+
+interface MenuItem {
+  name: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html'
+  templateUrl: './menu.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  menuItems: MenuItem[] = [
+    {
+      name: 'Fullscreen',
+      route: '/maps/fullscreen'
+    },
+    {
+      name: 'Zoom Range',
+      route: '/maps/zoom-range'
+    },
+    {
+      name: 'Markers',
+      route: '/maps/markers'
+    },
+    {
+      name: 'Properties',
+      route: '/maps/properties'
+    }
+  ];
 
 }
