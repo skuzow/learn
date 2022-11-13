a2d3774b20731b233a8de7f876bc1c3bd2389ccc
skuzow <alex@skuzow.net>
2022-11-13 23:44:28 +0100

    feat: maps-app: zoom map design

--- 09-maps-app/src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ 09-maps-app/src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 
 @Component({
   selector: 'app-zoom-range',
-  templateUrl: './zoom-range.component.html'
+  templateUrl: './zoom-range.component.html',
+  styles: [
+    `
+      .map-container {
+        height: 100%;
+        width: 100%;
+      }
+      
+      .row {
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        position: fixed;
+        z-index: 999;
+      }
+    `
+  ]
 })
 export class ZoomRangeComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+    new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [ -3.6829559362718958, 40.414294045943784 ],
+        zoom: 14
+    });
   }
 
 }
