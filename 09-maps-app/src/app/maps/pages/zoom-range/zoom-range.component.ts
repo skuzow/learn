2a9e4c79f0e22f5b96f92750384609faa973e0b0
skuzow <alex@skuzow.net>
2022-11-14 00:42:01 +0100

    feat: maps-app: zoom map buttons

--- 09-maps-app/src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ 09-maps-app/src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 
@@ -24,15 +24,29 @@ import * as mapboxgl from 'mapbox-gl';
     `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  ngOnInit(): void {
-    new mapboxgl.Map({
-        container: 'map',
+  @ViewChild('map') divMap!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel: number = 14;
+
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+        container: this.divMap.nativeElement,
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [ -3.6829559362718958, 40.414294045943784 ],
-        zoom: 14
+        zoom: this.zoomLevel
     });
+  }
+
+  zoomOut() {
+    this.map.zoomOut();
+    this.zoomLevel = this.map.getZoom();
+  }
+
+  zoomIn() {
+    this.map.zoomIn();
+    this.zoomLevel = this.map.getZoom();
   }
 
 }
