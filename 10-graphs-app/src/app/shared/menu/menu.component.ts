23da9c83e4f724830cb2eacfe363105f76eb1ed0
skuzow <alex@skuzow.net>
2022-11-25 23:50:30 +0100

    feat: graphs-app: menu

--- 10-graphs-app/src/app/shared/menu/menu.component.ts
+++ 10-graphs-app/src/app/shared/menu/menu.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+
+interface MenuItem {
+  name: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html'
+  templateUrl: './menu.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  menu: MenuItem[] = [
+    {
+      name: 'Bars',
+      route: '/graphs/bars'
+    },
+    {
+      name: 'Bars Double',
+      route: '/graphs/bars-double'
+    },
+    {
+      name: 'Dona',
+      route: '/graphs/dona'
+    },
+    {
+      name: 'Dona Http',
+      route: '/graphs/dona-http'
+    }
+  ]
 
 }
