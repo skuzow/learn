b9686706cf04be9f7c7a812c793fc4f04296f390
skuzow <alex@skuzow.net>
2022-12-06 20:13:20 +0100

    feat: graphs-app: dona graph

--- 10-graphs-app/src/app/graphs/pages/dona/dona.component.ts
+++ 10-graphs-app/src/app/graphs/pages/dona/dona.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html'
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  private colors: string[] = [ '#B24EF2', '#5F55E6', '#4E9AF2' ]
 
-  ngOnInit(): void {
-  }
+  public doughnutChartType: ChartType = 'doughnut';
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ],
+    datasets: [
+      { data: [ 350, 450, 100 ], backgroundColor: this.colors },
+      { data: [ 50, 150, 120 ], backgroundColor: this.colors },
+      { data: [ 250, 130, 70 ], backgroundColor: this.colors }
+    ]
+  };
 
 }
