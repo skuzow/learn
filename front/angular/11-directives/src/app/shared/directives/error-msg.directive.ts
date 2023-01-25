4185f01cabc1c443fe21eed5c8a6e808bb715abc
skuzow <alex@skuzow.net>
2023-01-25 14:11:52 +0100

    feat: angular/directives: error msg directive message & style

--- front/angular/11-directives/src/app/shared/directives/error-msg.directive.ts
+++ front/angular/11-directives/src/app/shared/directives/error-msg.directive.ts
@@ -6,18 +6,26 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class ErrorMsgDirective implements OnInit {
 
   private _htmlElement: ElementRef<HTMLElement>;
+
   @Input() color: string = 'red';
+  @Input() message: string = 'This field is required';
 
   constructor(private el: ElementRef<HTMLElement>) {
     this._htmlElement = el;
   }
 
   ngOnInit(): void {
-    this._setColor();
+    this._setStyle();
+    this._setMessage();
   }
 
-  private _setColor(): void {
+  private _setStyle(): void {
     this._htmlElement.nativeElement.style.color = this.color;
+    this._htmlElement.nativeElement.classList.add('form-text');
+  }
+
+  private _setMessage(): void {
+    this._htmlElement.nativeElement.innerText = this.message;
   }
 
 }
